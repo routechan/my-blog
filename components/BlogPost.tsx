a03297794580ecc9ffd7eda05dc95c93a80e3a7a
routechan <mr0326h14@gmail.com>
2025-02-25 21:20:57 +0900

    new

--- components/BlogPost.tsx
+++ components/BlogPost.tsx
@@ -1,29 +1,29 @@
 import React from 'react'
 import styles from '../src/styles/Home.module.scss';
 
-type BlogPostProps = {
-  title: string;
-  thumbnail: string;
-  publishedAt: string;
-  body: string;
+interface BlogPostProps {
+  post: {
+    title: string;
+    date: string;
+    content: string;
+    // 他に必要なプロパティがあれば追加してください
+  }
 }
 
-const BlogPost = ({ title, thumbnail, publishedAt, body }: BlogPostProps) => {
+export default function BlogPost({ post }: BlogPostProps) {
   return (
     <div>
       <div className='text-center'>
-        <div className={styles.thumbnail}>{thumbnail}</div>
+        <div className={styles.thumbnail}>{post.thumbnail}</div>
       </div>
-      <h1 className={styles.title}>{title}</h1>
-      <p className={styles.publishedAt}>{publishedAt}</p>
+      <h1 className={styles.title}>{post.title}</h1>
+      <p className={styles.publishedAt}>{post.date}</p>
       <div
         dangerouslySetInnerHTML={{
-          __html: `${body}`
+          __html: `${post.content}`
         }}
         className={styles.post}
       />
     </div>
   )
 }
-
-export default BlogPost
